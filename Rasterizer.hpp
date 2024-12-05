ec51d06ea78a130afd6f1bd079393a2cd421d71f
REZA GHOLIZADEH <e2490258@metu.edu.tr>
2024-12-06 00:35:57 +0300

    misc: delete unused code

--- Rasterizer.hpp
+++ Rasterizer.hpp
@@ -37,15 +37,5 @@ struct RasterTriangle
     std::vector<Fragment> out;
 };
 
-inline m::Matrix4 viewport_transformation(const ViewConfig& v)
-{
-    return Matrix4::from_rows({
-        {v.pixel_grid_rows / 2.0, 0,                          0, (v.pixel_grid_rows - 1) / 2.0   },
-        { 0,                      v.pixel_grid_columns / 2.0, 0, (v.pixel_grid_columns - 1) / 2.0},
-        { 0,                      0,                          1, 0                               },
-        { 0,                      0,                          0, 1                               }
-    });
-}
-
 RasterTriangle rasterize(const World& w, const ViewConfig& v,
                          size_t face_index);
